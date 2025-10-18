b4bb9343138e7d68a8cec1916741e36ed76418b8
Milad Foroughi <miladpro2020@gmail.com>
2025-10-18 17:17:36 +0330

    Make Order

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -19,22 +19,65 @@ const addOrderItems = asyncHandler(async (req, res) => {
   if (orderItems && orderItems.length === 0) {
     res.status(404)
     throw new Error('No order items')
-  } else {
-    const order = new Order({
-      orderItems,
-      user: req.user._id,
-      shippingAddress,
-      paymentMethod,
-      itemsPrice,
-      taxPrice,
-      shippingPrice,
-      totalPrice,
-    })
   }
+  const order = new Order({
+    orderItems,
+    user: req.user._id,
+    shippingAddress,
+    paymentMethod,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+  })
 
   const createdOrder = await order.save()
 
   res.status(201).json(createdOrder)
 })
 
-export { addOrderItems }
+// @desc Get order by ID
+// @router GET /api/orders/:id
+// @access Private
+
+const getOrderById = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id).populate(
+    'user',
+    'name email'
+  )
+
+  if (order) {
+    res.json(order)
+  } else {
+    res.status(404)
+    throw new Error('Order not found')
+  }
+})
+
+// @desc Update order to paid
+// @router GET /api/orders/:id/pay
+// @access Private
+
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+  const order = await Order.findBytId(req.params.id)
+
+  if (order) {
+    order.isPaid = true
+    order.paidAt = Date.now()
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.payer.email_address,
+    }
+
+    const updatedOrder = await order.save()
+
+    res.json(updatedOrder)
+  } else {
+    res.status(404)
+    throw new Error('Order not found')
+  }
+})
+
+export { addOrderItems, getOrderById, updateOrderToPaid }
